67f2a1fcb916481845c20b8c794833027e2a72cc
port <108868128+portdeveloper@users.noreply.github.com>
2023-10-31 23:09:11 +0300

    Make the app logo + name clickable

--- packages/nextjs/components/address-vision/Navbar.tsx
+++ packages/nextjs/components/address-vision/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { Address } from "viem";
 import { AddressInput } from "~~/components/scaffold-eth";
 
@@ -7,9 +8,19 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ searchedAddress, setSearchedAddress }: NavbarProps) => {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    setSearchedAddress("");
+    router.push("/", undefined, { shallow: true });
+  };
+
   return (
     <div className="navbar sticky top-0 z-20 grid min-h-0 flex-shrink-0 grid-cols-12 justify-between bg-base-100 px-0 shadow-md shadow-secondary sm:px-2 lg:static">
-      <div className="col-start-4 flex flex-row items-center md:col-start-1 md:col-end-3">
+      <div
+        onClick={handleLogoClick}
+        className="col-start-4 flex flex-row items-center md:col-start-1 md:col-end-3 cursor-pointer"
+      >
         <div className="mb-4 text-4xl">👀</div>
         <h1 className="ml-2 text-2xl font-bold">address.vision</h1>
       </div>
