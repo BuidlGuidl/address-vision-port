8f0c5e6ba02364412265907710f1a8fb3ca994f2
port <108868128+portdeveloper@users.noreply.github.com>
2023-10-31 23:36:34 +0300

    Fix logo click on larger screens

--- packages/nextjs/components/address-vision/Navbar.tsx
+++ packages/nextjs/components/address-vision/Navbar.tsx
@@ -17,12 +17,13 @@ export const Navbar = ({ searchedAddress, setSearchedAddress }: NavbarProps) => 
 
   return (
     <div className="navbar sticky top-0 z-20 grid min-h-0 flex-shrink-0 grid-cols-12 justify-between bg-base-100 px-0 shadow-md shadow-secondary sm:px-2 lg:static">
-      <div
-        onClick={handleLogoClick}
-        className="col-start-4 flex flex-row items-center md:col-start-1 md:col-end-3 cursor-pointer"
-      >
-        <div className="mb-4 text-4xl">👀</div>
-        <h1 className="ml-2 text-2xl font-bold">address.vision</h1>
+      <div className="col-start-4 flex flex-row items-center md:col-start-1 md:col-end-3">
+        <div onClick={handleLogoClick} className="mb-4 text-4xl cursor-pointer">
+          👀
+        </div>
+        <h1 onClick={handleLogoClick} className="ml-2 text-2xl font-bold cursor-pointer">
+          address.vision
+        </h1>
       </div>
       <div className="col-start-2 col-end-12 row-start-2 flex justify-center md:col-start-4 md:col-end-10 md:row-auto">
         <div className="flex-grow">
