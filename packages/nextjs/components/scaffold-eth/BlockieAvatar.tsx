9518a0b371dea79c4ace4a551d2054dc55f71730
port <108868128+portdeveloper@users.noreply.github.com>
2024-03-09 16:26:09 +0300

    Modify BlockieAvatar to use object-cover

--- packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
+++ packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
@@ -1,15 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import { AvatarComponent } from "@rainbow-me/rainbowkit";
 import { blo } from "blo";
 
-// Custom Avatar for RainbowKit
 export const BlockieAvatar: AvatarComponent = ({ address, ensImage, size }) => (
-  // Don't want to use nextJS Image here (and adding remote patterns for the URL)
-  // eslint-disable-next-line @next/next/no-img-element
-  <img
-    className="rounded-full"
-    src={ensImage || blo(address as `0x${string}`)}
-    width={size}
-    height={size}
-    alt={`${address} avatar`}
-  />
+  <div className={`h-${size} w-${size} overflow-hidden rounded-full bg-gray-100 flex items-center justify-center`}>
+    <img
+      className="object-cover min-w-full min-h-full"
+      src={ensImage || blo(address as `0x${string}`)}
+      alt={`${address} avatar`}
+    />
+  </div>
 );
