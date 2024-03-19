6e9e0d7320a236baf9c9bd84ce751b0ca45dde37
port <108868128+portdeveloper@users.noreply.github.com>
2024-03-19 21:15:13 +0300

    Fix wrong text sizes on address card

--- packages/nextjs/components/scaffold-eth/Address.tsx
+++ packages/nextjs/components/scaffold-eth/Address.tsx
@@ -149,9 +149,9 @@ export const Address = ({
   if (ens && ens?.length > 22) {
     textDisplaySize = "lg";
   } else if (ens && ens?.length > 17) {
-    textDisplaySize = "3xl";
-  } else if (ens && ens?.length > 12) {
     textDisplaySize = "2xl";
+  } else if (ens && ens?.length > 12) {
+    textDisplaySize = "3xl";
   }
 
   let iconSizes = "h-6 w-6";
